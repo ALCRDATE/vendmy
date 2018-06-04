3e3e513718096f17b19f34b6697e2141fc936402
ALCRDATE <Khalil.cardate@gmail.com>
2018-06-04 18:00:27 +0000

    Welcome Page
    Modification in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel with GitHub upgraded
+                    Laravel with GitHub
                 </div>
 
                 <div class="links">
